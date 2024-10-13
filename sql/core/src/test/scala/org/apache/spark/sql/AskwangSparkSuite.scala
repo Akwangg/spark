d8cddb9b9d1119d528a67cdb199c8cc1a733488a
Askwang <135721692+Askwang@users.noreply.github.com>
2024-10-13 19:54:55 +0800

    fix env

--- sql/core/src/test/scala/org/apache/spark/sql/AskwangSparkSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/AskwangSparkSuite.scala
@@ -30,7 +30,6 @@ class AskwangSparkSuite extends QueryTest with SharedSparkSession with AdaptiveS
   setupTestData()
 
   test("create table/create table like/create table as select") {
-    // todo: test failed. 无效的源发行版: 17.
     withTable("tb") {
       withSQLConf(SQLConf.PLAN_CHANGE_LOG_LEVEL.key -> "INFO",
         SQLConf.DATETIME_JAVA8API_ENABLED.key -> "true") {
